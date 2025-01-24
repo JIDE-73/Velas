2eab9bdacbacb50b32c6d482071c876028e5414b
JIDE-73 <167656483+JIDE-73@users.noreply.github.com>
2025-01-23 22:56:32 -0800

    bug solving 5

--- Front_Velas/src/Routes.tsx
+++ Front_Velas/src/Routes.tsx
@@ -1,9 +1,4 @@
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
@@ -13,14 +8,13 @@ import Footer from "./components/Footer";
 function AppRoutes() {
   return (
     <Router basename="/Velas/">
-      <Navbar /> {/* Menú fijo en la parte superior */}
+      <Navbar />
       <div className="min-h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
-          {/* Redirige a la página de inicio si la ruta no existe */}
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="*" element={<h2>Página no encontrada</h2>} />
         </Routes>
       </div>
       <Footer />
